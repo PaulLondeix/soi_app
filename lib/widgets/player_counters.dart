9189cb30255cf4968d76540b5dff6a99efb4246d
PaulLondeix <paul.londeix@gmail.com>
2023-02-24 18:33:06 +0100

    Counters Screen prettier

--- lib/widgets/player_counters.dart
+++ lib/widgets/player_counters.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/player.dart';
+import '../enums/character.dart';
 
 class PlayerCounter extends StatefulWidget {
   final Player player;
@@ -13,31 +14,56 @@ class _PlayerCounterState extends State<PlayerCounter> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black,
+          width: 1,
+        ),
+        image: DecorationImage(
+          image: AssetImage(widget.player.character.imagePath),
+          fit: BoxFit.cover,
+        ),
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
             widget.player.character.name,
-            style: TextStyle(fontSize: 24),
+            style: TextStyle(fontSize: 24, color: Colors.white),
           ),
           SizedBox(height: 10),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               IconButton(
-                icon: Icon(Icons.remove),
+                icon: Icon(
+                  Icons.remove,
+                  color: Colors.white,
+                ),
                 onPressed: () {
                   setState(() {
                     widget.player.decrementLife();
                   });
                 },
               ),
-              Text(
-                "${widget.player.life}",
-                style: TextStyle(fontSize: 18),
+              Container(
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/life.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Text(
+                  "${widget.player.life}",
+                  style: TextStyle(fontSize: 18),
+                ),
               ),
               IconButton(
-                icon: Icon(Icons.add),
+                icon: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
                 onPressed: () {
                   setState(() {
                     widget.player.incrementLife();
@@ -51,19 +77,34 @@ class _PlayerCounterState extends State<PlayerCounter> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               IconButton(
-                icon: Icon(Icons.remove),
+                icon: Icon(
+                  Icons.remove,
+                  color: Colors.white,
+                ),
                 onPressed: () {
                   setState(() {
                     widget.player.decrementMastery();
                   });
                 },
               ),
-              Text(
-                "${widget.player.mastery}",
-                style: TextStyle(fontSize: 18),
+              Container(
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/mastery.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Text(
+                  "${widget.player.mastery}",
+                  style: TextStyle(fontSize: 18),
+                ),
               ),
               IconButton(
-                icon: Icon(Icons.add),
+                icon: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
                 onPressed: () {
                   setState(() {
                     widget.player.incrementMastery();
